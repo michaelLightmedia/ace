6af375012a3b673ff05a211a2ad9ca46d0d0f0a3
Programmer <programmer@PHILWEBSERVICES-Mac-Judan.local>
2014-05-23 08:22:47 +0800

    update ace

--- app/modules/media/routes.php
+++ app/modules/media/routes.php
@@ -3,15 +3,15 @@
 Route::group(array('before' => 'auth'), function(){
 
 	//display all media
-	Route::any('admin/media','App\Modules\Media\MediaController@indexAction')->before('media_manage_permission');
+	Route::any('admin/media','App\Modules\Media\MediaController@indexAction');//->before('media_manage_permission');
 	//show media in modal
 	Route::any('media/modal','App\Modules\Media\MediaController@modalAction');
 	//create new media
-	Route::any('admin/media/create', 'App\Modules\Media\MediaController@createAction')->before('media_create_permission');
+	Route::any('admin/media/create', 'App\Modules\Media\MediaController@createAction');//->before('media_create_permission');
 	//edit media files
-	Route::any('admin/media/{id}/edit','App\Modules\Media\MediaController@editAction')->before('media_edit_permission');
+	Route::any('admin/media/{id}/edit','App\Modules\Media\MediaController@editAction');//->before('media_edit_permission');
 	//delete media files
-	Route::any('admin/media/delete', 'App\Modules\Media\MediaController@deleteAction')->before('media_delete_permission');
+	Route::any('admin/media/delete', 'App\Modules\Media\MediaController@deleteAction');//->before('media_delete_permission');
 	//display file uploader
 	Route::any('admin/media/uploader', 'App\Modules\Media\MediaController@uploaderAction');
 	//show all media files in modal
